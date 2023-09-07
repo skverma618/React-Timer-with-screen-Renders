6b12b9a2cd9cbdef0eb6c860db15f459a136de6d
Sushil Kumar <sushilkumarskp618@gmail.com>
2023-09-07 22:25:20 +0530

    useRef

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [randomInput, setRandomInput] = useState('');
+    const [seconds, setSeconds] = useState(0);
+
+    const renders = useRef(0);
+    const inputRef = useRef();
+    const timerId = useRef();
+
+    const handleChange = (e) => {
+        setRandomInput(e.target.value);
+        renders.current++;
+    }
+
+    const startTimer = () => {
+        if(timerId.current) return; // If there already is a timer running, do nothing, otherwise, multiple setinterval will be created and the timer will run faster
+         timerId.current = setInterval( () => {
+             renders.current++;
+             setSeconds( seconds => seconds + 1);
+         }, 1000)
+        inputRef.current.focus();
+    }
+
+    const stopTimer = () => {
+        clearInterval(timerId.current);
+        timerId.current = 0;
+    }
+
+    const resetTimer = () => {
+        stopTimer();
+        if(seconds > 0){
+            renders.current++;
+            setSeconds(0);
+        }
+    }
+
+    return (
+        <main className="App">
+            <input
+                ref={inputRef}
+                type="text"
+                value={randomInput}
+                placeholder="Random Input"
+                onChange={handleChange}
+            />
+            <p>Renders: {renders.current}</p>
+            <br /><br />
+            <section>
+                <button onClick={startTimer}>Start</button>
+                <button onClick={stopTimer}>Stop</button>
+                <button onClick={resetTimer}>Reset</button>
+            </section>
+            <br /><br />
+            <p>Seconds: {seconds}</p>
+            <br /><br />
+            <p>{randomInput}</p>
+        </main>
+    );
 }
 
 export default App;
